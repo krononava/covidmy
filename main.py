9fe7db464ac70bcac9e27c5dcb667104b8029298
Lenix <35059807+lenix3347@users.noreply.github.com>
2021-08-23 04:39:19 +0000

    removed channel all completely

--- main.py
+++ main.py
@@ -53,11 +53,9 @@ async def on_message(message):
             CoronaEmbed.add_field(name="Last Updated :", value=getApi('lastUpdated'), inline=False)
             await message.channel.send("auto updates enabled")
             await message.channel.send(embed = CoronaEmbed)
-            channel = ""
-            channel.append(message.channel.id)
             with open('channel.json', 'r') as file:
                 data = json.load(file)
-            data.append(channel)
+            data.append(message.channel.id)
             with open('channel.json', 'w') as file:
                 json.dump(data, file)
 
