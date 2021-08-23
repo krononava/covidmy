3b70a3110d7c8347947cd7b9d3716f2e78b734ad
Lenix <35059807+lenix3347@users.noreply.github.com>
2021-08-23 04:28:49 +0000

    no more function

--- main.py
+++ main.py
@@ -34,38 +34,6 @@ def checkCases():
                 initial = getApi("dailyCases")
                 return True
 
-def jsonDuplicate():
-    f = open('channel.json', 'r')
-    data = json.load(f)
-    if message.channel.id in data:
-        return True
-    else:
-        return False
-
-def writeJson():
-    channel = None
-    channel.append(message.channel.id)
-    with open('channel.json', 'r') as file:
-        data = json.load(file)
-    data.append(channel)
-    with open('channel.json', 'w') as file:
-        json.dump(data, file)
-
-def removeJson():
-    with open('channel.json', 'r') as file:
-        data = json.load(file)
-    data.pop(message.channel.id, "not registered")
-    with open('channel.json', 'w') as file:
-        json.dump(data, file)
-
-def autoSend():
-    f = open('channel.json', 'r')
-    data = json.load(f)
-    for i in data:
-        if (message.channel.id == i): 
-            message.channel.send(embed = CoronaEmbed)
-    f.close()
-
 client = discord.Client()
 
 @client.event
@@ -74,7 +42,9 @@ async def on_message(message):
         return
 
     if message.content.startswith("`awakenmycovid"):
-        if (jsonDuplicate() == True):
+        f = open('channel.json', 'r')
+        data = json.load(f)
+        if message.channel.id in data:
             await message.channel.send("channel auto updates already set")
         else:
             CoronaEmbed = discord.Embed(title="The Coconut Destiny", description="[Daily Report of Covid-19](https://www.coronatracker.com/country/malaysia/)", color=0xff0000)
@@ -83,14 +53,33 @@ async def on_message(message):
             CoronaEmbed.add_field(name="Last Updated :", value=getApi('lastUpdated'), inline=False)
             await message.channel.send("auto updates enabled")
             await message.channel.send(embed = CoronaEmbed)
-            writeJson()
+            channel = None
+            channel.append(message.channel.id)
+            with open('channel.json', 'r') as file:
+                data = json.load(file)
+            data.append(channel)
+            with open('channel.json', 'w') as file:
+                json.dump(data, file)
 
     if message.content.startswith("`slumbermycovid"):
-        removeJson()
+        with open('channel.json', 'r') as file:
+            data = json.load(file)
+        data.pop(message.channel.id, "not registered")
+        with open('channel.json', 'w') as file:
+            json.dump(data, file)
         await message.channel.send("auto updates disabled")
 
     if (checkCases() == True):
-        autoSend()
+        f = open('channel.json', 'r')
+        data = json.load(f)
+        for i in data:
+            if (message.channel.id == i):
+                CoronaEmbed = discord.Embed(title="The Coconut Destiny", description="[Daily Report of Covid-19](https://www.coronatracker.com/country/malaysia/)", color=0xff0000)
+                CoronaEmbed.add_field(name="Cases :", value=getApi('dailyCases'), inline=False)
+                CoronaEmbed.add_field(name="Death :", value=getApi('dailyDeath'), inline=False)
+                CoronaEmbed.add_field(name="Last Updated :", value=getApi('lastUpdated'), inline=False)
+                await message.channel.send(embed = CoronaEmbed)
+        f.close()
 	
 keep_alive()
 client.run(os.environ['token'])
